fabc32b283b02fa7dff3b46bdc1e218e7cd82993
CrocodileGenna <99128333+CrocodileGenna@users.noreply.github.com>
2022-09-20 19:22:56 +0000

    Deploying to gh-pages from @ CrocodileGenna/goit-js-hw-09@295983bc19bc49a199065c2f13fdeb3312506b25 🚀

--- 03-promises.96ce9b58.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]");
-//# sourceMappingURL=03-promises.96ce9b58.js.map
--- 03-promises.c2ef30e3.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]");
-//# sourceMappingURL=03-promises.c2ef30e3.js.map
